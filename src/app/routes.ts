33b829fae830f39787ba2e8e317fd0dd86db4608
Arthur Felipe <arthurfelipe5567@gmail.com>
2025-09-02 17:23:29 -0300

    feat: add workout creation unfinished page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,14 +6,17 @@ import {
 } from '@react-router/dev/routes'
 
 export default [
-  route('login', 'routes/login.tsx', { id: 'Login' }),
-  route('signup', 'routes/signup.tsx', { id: 'Signup' }),
+  route('login', 'routes/auth/login.tsx', { id: 'Login' }),
+  route('signup', 'routes/auth/signup.tsx', { id: 'Signup' }),
   route('confirmed', 'routes/confirmed.tsx', { id: 'Confirmed' }),
   layout('layouts/protected-route.tsx', [
     layout('layouts/layout.tsx', [
       index('routes/home.tsx', {
         id: 'Home'
-      })
+      }),
+      route('workout', 'routes/workout/workout.tsx', { id: 'Workout' }, [
+        route('create', 'routes/workout/workout-create.tsx', { id: 'Workout-Create' })
+      ])
     ])
   ])
 ] satisfies RouteConfig
